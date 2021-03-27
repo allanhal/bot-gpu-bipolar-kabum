b7632998adad828ada8daef5f97d66a55ab39ecc
Allan <allan.hal@gmail.com>
2021-03-27 04:49:20 -0300

    Draft on stock checking

--- app.js
+++ app.js
@@ -1,49 +1,25 @@
-const config = require('./config');
-const puppeteer = require('puppeteer');
+const config = require("./config");
+const puppeteer = require("puppeteer");
 
-const Twit = require('twit');
-const Kabum = require('./kabum');
-const T = new Twit(config);
+const Kabum = require("./kabum");
 
-const tracked_account_id = ''; // PERSONAL ACCOUNT ID
-//const tracked_account_id = '1285199873890045953'; // GPU BIPOLAR
+const tracked_words = ["RTX 2080 TI", "RTX 2070 SUPER", "RTX 3070", "RTX 3080"];
 
-const tracked_words = [
-  'RTX 2080 TI',
-  'RTX 2070 SUPER',
-  'RTX 3070',
-  'RTX 3080'
-];
-
-main = () => {
-  const stream = T.stream('statuses/filter', { follow: [tracked_account_id] });
-  stream.on('tweet', async function (tweet) {
-    if(tweet.user.id_str !== tracked_account_id) return;
-    let text_lower_case = '';
-    try {
-      text_lower_case = tweet.extended_tweet.full_text.toLowerCase();
-      console.log(text_lower_case);
-    } catch(err) {
-      console.log('[CRITICAL ERROR] OCORREU UM ERRO NO ACESSO AO TWEET: ', err);
+main = async () => {
+  for (const word of tracked_words) {
+    const regex = new RegExp(".*" + word.toLowerCase() + ".*");
+    const match = text_lower_case.match(regex);
+    if (match.length > 0) {
+      console.log("achou");
+      await iniciarBrowser(match[0], word);
+      break;
     }
-    
-    if(text_lower_case !== '') {
-      for(const word of tracked_words) {
-        const regex = new RegExp('.*'+word.toLowerCase()+'.*');
-        const match = text_lower_case.match(regex);
-        if(match.length > 0) {
-          console.log('achou');
-          await iniciarBrowser(match[0], word);
-          break;
-        }
-      }
-    }
-  });
-}
+  }
+};
 
 iniciarBrowser = async (text_search, selected_word) => {
   const browser = await puppeteer.launch({
-    product: 'chrome',
+    product: "chrome",
     headless: false,
   });
   const page = await browser.newPage();
@@ -54,23 +30,31 @@ iniciarBrowser = async (text_search, selected_word) => {
 
   await kabum.search(text_search);
 
-  await kabum.open_product(text_search);
+  await kabum.cheapAvailable(selected_word);
 
-  await kabum.check_price(text_search, selected_word);
+  // await kabum.open_product(text_search);
 
-  await kabum.add_to_cart(text_search);
+  // await kabum.check_price(text_search, selected_word);
 
-  await kabum.next_cart_page();
+  // await kabum.add_to_cart(text_search);
 
-  await kabum.add_coupon();
+  // await kabum.next_cart_page();
 
-  await kabum.checkout();
+  // await kabum.add_coupon();
 
-  await kabum.confirm_data_after_checkout();
+  // await kabum.checkout();
+
+  // await kabum.confirm_data_after_checkout();
 
   //await kabum.end_checkout();
-}
+};
 
-console.log('[BEGIN] APLICAÇÃO INICIADA. PROCURANDO POR PROMOÇÕES DAS GPUs 2070 SUPER, 2080 TI, 3070 e 3080 de acordo com os tweets do perfil GPU BIPOLAR no Twitter!');
-//main();
-iniciarBrowser('EVGA GeForce RTX 2070 Super XC Gaming', tracked_words[0]);
+console.log(
+  "[BEGIN] APLICAÇÃO INICIADA. PROCURANDO POR PROMOÇÕES DAS GPUs 2070 SUPER, 2080 TI, 3070 e 3080 de acordo com os tweets do perfil GPU BIPOLAR no Twitter!"
+);
+// main();
+(async () => {
+  // main();
+  // iniciarBrowser("EVGA GeForce RTX 2070 Super XC Gaming", tracked_words[0]);
+  iniciarBrowser("rtx 3060", "3060");
+})();
--- kabum.js
+++ kabum.js
@@ -1,24 +1,15 @@
-const cookiesFilePath = './cookies.json';
-const jsonfile = require('jsonfile')
-const fileExists = require('./existsSync')
-const secrets = require('./secrets');
+const cookiesFilePath = "./cookies.json";
+const jsonfile = require("jsonfile");
+const fileExists = require("./existsSync");
+const secrets = require("./secrets");
 
-const url_kabum = 'https://www.kabum.com.br';
+const url_kabum = "https://www.kabum.com.br";
 
-const tracked_words = [
-  'RTX 2080 TI',
-  'RTX 2070 SUPER',
-  'RTX 3070',
-  'RTX 3080'
-];
+const tracked_words = ["RTX 2080 TI", "RTX 2070 SUPER", "RTX 3070", "RTX 3080"];
 
-const coupons = [
-  'EMERSONBR',
-  'ALANZOKA'
-]
+const coupons = ["EMERSONBR", "ALANZOKA"];
 
 class Kabum {
-
   constructor(page, browser) {
     this.page = page;
     this.browser = browser;
@@ -27,14 +18,14 @@ class Kabum {
   async start() {
     await this.loadSession();
     await this.page.setDefaultNavigationTimeout(60000);
-    await this.page.goto(url_kabum, {waitUntil: 'domcontentloaded'});
+    await this.page.goto(url_kabum, { waitUntil: "domcontentloaded" });
 
     const sessionIsLoaded = await this.sessionIsLoaded();
     console.log(sessionIsLoaded);
-    if(!sessionIsLoaded) {
-      await this.login();
-      await this.saveSession();
-    }
+    // if(!sessionIsLoaded) {
+    //   await this.login();
+    //   await this.saveSession();
+    // }
   }
 
   async loadSession() {
@@ -43,127 +34,195 @@ class Kabum {
       const cookiesArr = require(cookiesFilePath);
       if (cookiesArr.length !== 0) {
         for (let cookie of cookiesArr) {
-          await this.page.setCookie(cookie)
+          await this.page.setCookie(cookie);
         }
-        console.log('[INFO] SESSÃO CARREGADA NO BROWSER.');
+        console.log("[INFO] SESSÃO CARREGADA NO BROWSER.");
       }
     }
   }
 
   async saveSession() {
-    const cookiesObject = await this.page.cookies()
-    jsonfile.writeFile(cookiesFilePath, cookiesObject, { spaces: 2 },
-      (err) => { 
-      if (err) console.log('[ERROR] ERRO AO ESCREVER O ARQUIVO DE COOKIES: ', err);
+    const cookiesObject = await this.page.cookies();
+    jsonfile.writeFile(cookiesFilePath, cookiesObject, { spaces: 2 }, (err) => {
+      if (err)
+        console.log("[ERROR] ERRO AO ESCREVER O ARQUIVO DE COOKIES: ", err);
 
-      console.log('[INFO] SESSÃO SALVA COM SUCESSO.')
-    })
+      console.log("[INFO] SESSÃO SALVA COM SUCESSO.");
+    });
   }
 
   async sessionIsLoaded() {
-    return await this.page.$('a[href="https://www.kabum.com.br/cgi-local/site/login/login.cgi"]') === null;
+    return (
+      (await this.page.$(
+        'a[href="https://www.kabum.com.br/cgi-local/site/login/login.cgi"]'
+      )) === null
+    );
   }
 
   async login() {
-    this.page.click('a[href="https://www.kabum.com.br/cgi-local/site/login/login.cgi"]');
+    this.page.click(
+      'a[href="https://www.kabum.com.br/cgi-local/site/login/login.cgi"]'
+    );
     await this.page.waitForNavigation();
 
-    await this.page.type('#textfield12', secrets.email);
+    await this.page.type("#textfield12", secrets.email);
     await this.page.type('[name="senha"]', secrets.password);
 
-    this.page.click('input[src="https://static.kabum.com.br/conteudo/temas/001/imagens/login/botao_logar.png"]');
+    this.page.click(
+      'input[src="https://static.kabum.com.br/conteudo/temas/001/imagens/login/botao_logar.png"]'
+    );
     await this.page.waitForNavigation();
   }
 
   async search(text_search) {
-    await this.page.waitForSelector('.sprocura');
-    await this.page.type('.sprocura', text_search);
-      
-    this.page.click('#bt-busca');
+    await this.page.waitForSelector(".sprocura");
+    await this.page.type(".sprocura", text_search);
+
+    this.page.click("#bt-busca");
     await this.page.waitForNavigation();
+  }
+
+  async cheapAvailable(text_search) {
+    await this.page.waitForSelector("#listagem-produtos");
+
+    const itemNome = await this.page.$eval(
+      ".item-nome",
+      (element) => element.innerText
+    );
+    const disponivel = await this.page.$eval(
+      `img[alt="botao comprar"]`,
+      (element) => {
+        console.log("element", element);
+        return element.src;
+      }
+    );
+    if (itemNome.includes(text_search) && disponivel.includes("comprar")) {
+      console.log("disponivel");
+    }
   }
 
   async open_product(text_search) {
     try {
-      this.page.click('#listagem-produtos a');
-       await this.page.waitForNavigation();
+      this.page.click("#listagem-produtos a");
+      await this.page.waitForNavigation();
     } catch (err) {
       console.log("[ERROR] PESQUISA NAO ENCONTROU RESULTADOS - " + text_search);
-			this.browser.close();
-			throw err;
-    };
+      this.browser.close();
+      throw err;
+    }
   }
 
   async check_price(text_search, selected_word) {
-    const preco_limite_2080 = parseFloat('5.000,00');
-    const preco_limite_3080 = parseFloat('6.000,00');
-    const preco_limite_2070 = parseFloat('3.000,00');
-    const preco_limite_3070 = parseFloat('5.000,00');
+    const preco_limite_2080 = parseFloat("5.000,00");
+    const preco_limite_3080 = parseFloat("6.000,00");
+    const preco_limite_2070 = parseFloat("3.000,00");
+    const preco_limite_3070 = parseFloat("5.000,00");
 
     let priceFloat = 0;
 
     try {
-      const priceText = await this.page.$eval('.preco_desconto_avista-cm, .preco_desconto > span > span > strong', element => element.innerText);
-      priceFloat = parseFloat(priceText.split(' ')[1]);
-    
-      if(selected_word === tracked_words[1]) {
-        if(priceFloat > preco_limite_2070) {
-          console.log("[ERROR] O PREÇO DO PRODUTO ESTÁ ACIMA DO LIMITE - " + text_search + " (" + priceFloat + ")");
-					this.browser.close();
-					throw err;
-					return;
+      const priceText = await this.page.$eval(
+        ".preco_desconto_avista-cm, .preco_desconto > span > span > strong",
+        (element) => element.innerText
+      );
+      priceFloat = parseFloat(priceText.split(" ")[1]);
+
+      if (selected_word === tracked_words[1]) {
+        if (priceFloat > preco_limite_2070) {
+          console.log(
+            "[ERROR] O PREÇO DO PRODUTO ESTÁ ACIMA DO LIMITE - " +
+              text_search +
+              " (" +
+              priceFloat +
+              ")"
+          );
+          this.browser.close();
+          throw err;
+          return;
         }
-      } else if(selected_word === tracked_words[0]) {
-        if(priceFloat > preco_limite_2080) {
-          console.log("[ERROR] O PREÇO DO PRODUTO ESTÁ ACIMA DO LIMITE - " + text_search + " (" + priceFloat + ")");
-					this.browser.close();
-					throw err;
-					return;
+      } else if (selected_word === tracked_words[0]) {
+        if (priceFloat > preco_limite_2080) {
+          console.log(
+            "[ERROR] O PREÇO DO PRODUTO ESTÁ ACIMA DO LIMITE - " +
+              text_search +
+              " (" +
+              priceFloat +
+              ")"
+          );
+          this.browser.close();
+          throw err;
+          return;
         }
-      } else if(selected_word === tracked_words[3]) {
-          if(priceFloat > preco_limite_3080) {
-            console.log("[ERROR] O PREÇO DO PRODUTO ESTÁ ACIMA DO LIMITE - " + text_search + " (" + priceFloat + ")");
-						this.browser.close();
-						throw err;
-						return;
-          }
-      } else if(selected_word === tracked_words[2]) {
-        if(priceFloat > preco_limite_3070) {
-          console.log("[ERROR] O PREÇO DO PRODUTO ESTÁ ACIMA DO LIMITE - " + text_search + " (" + priceFloat + ")");
-					this.browser.close();
-					throw err;
-					return;
+      } else if (selected_word === tracked_words[3]) {
+        if (priceFloat > preco_limite_3080) {
+          console.log(
+            "[ERROR] O PREÇO DO PRODUTO ESTÁ ACIMA DO LIMITE - " +
+              text_search +
+              " (" +
+              priceFloat +
+              ")"
+          );
+          this.browser.close();
+          throw err;
+          return;
+        }
+      } else if (selected_word === tracked_words[2]) {
+        if (priceFloat > preco_limite_3070) {
+          console.log(
+            "[ERROR] O PREÇO DO PRODUTO ESTÁ ACIMA DO LIMITE - " +
+              text_search +
+              " (" +
+              priceFloat +
+              ")"
+          );
+          this.browser.close();
+          throw err;
+          return;
         }
       }
 
-      console.log("[INFO] PREÇO DO PRODUTO VÁLIDO PARA O LIMITE DA CATEGORIA (" + priceFloat + ") - " + text_search);
+      console.log(
+        "[INFO] PREÇO DO PRODUTO VÁLIDO PARA O LIMITE DA CATEGORIA (" +
+          priceFloat +
+          ") - " +
+          text_search
+      );
     } catch (err) {
-      console.log("[ERROR] ERRO AO TENTAR RECUPERAR O PREÇO DO PRODUTO - " + text_search);
-			this.browser.close();
-			throw err;
+      console.log(
+        "[ERROR] ERRO AO TENTAR RECUPERAR O PREÇO DO PRODUTO - " + text_search
+      );
+      this.browser.close();
+      throw err;
     }
   }
 
   async add_to_cart(text_search) {
     try {
-      this.page.click('.botao-comprar');
+      this.page.click(".botao-comprar");
       await this.page.waitForNavigation();
     } catch (err) {
-			console.log("[ESGOTADA] " + new Date().toLocaleString() + " - " + text_search);
-			this.browser.close();
-			throw err;
+      console.log(
+        "[ESGOTADA] " + new Date().toLocaleString() + " - " + text_search
+      );
+      this.browser.close();
+      throw err;
     }
   }
 
   async next_cart_page() {
-		try {
-			this.page.click('a[href="https://www.kabum.com.br/cgi-local/site/carrinho/carrinho.cgi"]');
-			await this.page.waitForNavigation();
-		} catch(err) {
-			console.log('[ERROR] OCORREU UM ERRO AO CLICAR EM PROCEDER NO PARA CHECKOUT.', err);
-			this.browser.close();
-			throw err;
-		}
+    try {
+      this.page.click(
+        'a[href="https://www.kabum.com.br/cgi-local/site/carrinho/carrinho.cgi"]'
+      );
+      await this.page.waitForNavigation();
+    } catch (err) {
+      console.log(
+        "[ERROR] OCORREU UM ERRO AO CLICAR EM PROCEDER NO PARA CHECKOUT.",
+        err
+      );
+      this.browser.close();
+      throw err;
+    }
   }
 
   async add_coupon() {
@@ -173,14 +232,18 @@ class Kabum {
   }
 
   async checkout() {
-    const [buttonFinalizarCompraPrev] = await this.page.$x("//button[contains(., 'Finalizar Compra')]");
-    if(buttonFinalizarCompraPrev) {
+    const [buttonFinalizarCompraPrev] = await this.page.$x(
+      "//button[contains(., 'Finalizar Compra')]"
+    );
+    if (buttonFinalizarCompraPrev) {
       buttonFinalizarCompraPrev.click();
       await this.page.waitForNavigation();
     } else {
-      console.log("[ERROR] Ocorreu um erro ao clicar no botão de Finalizar Compra 1");
-			this.browser.close();
-			throw err;
+      console.log(
+        "[ERROR] Ocorreu um erro ao clicar no botão de Finalizar Compra 1"
+      );
+      this.browser.close();
+      throw err;
     }
   }
 
@@ -199,8 +262,8 @@ class Kabum {
   }
 
   delay(time) {
-    return new Promise(function(resolve) { 
-			setTimeout(resolve, time)
+    return new Promise(function (resolve) {
+      setTimeout(resolve, time);
     });
   }
 }
